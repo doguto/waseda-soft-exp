77aba918d30ebf085a99dd3cde75763da8a2fdd0
Kota-James <n.kota.all.01@gmail.com>
2026-05-17 03:55:48 +0900

    feat: implement EndDiscussionService with refactoring comments

--- src/server/service/EndDiscussionService.java
+++ src/server/service/EndDiscussionService.java
@@ -2,6 +2,8 @@ package src.server.service;
 
 import src.message.EndDiscussionMessage;
 import src.message.EndDiscussionResultMessage;
+import src.server.core.ServiceType;
+import src.server.game.GameEvent;
 import src.server.game.GameMaster;
 
 public class EndDiscussionService extends BaseService {
@@ -12,8 +14,22 @@ public class EndDiscussionService extends BaseService {
 
     public EndDiscussionResultMessage call(EndDiscussionMessage msg) {
         // stateManager.check(GameEvent.DISCUSSION_ENDED) を呼んでフェーズを VOTE に遷移させる
+        stateManager.check(GameEvent.DISCUSSION_ENDED);
         //   → discussionEnded フラグで二重呼び出しを防止済み
         // gameMaster.pushService(ServiceType.VOTE_PHASE_START) をキューに積む
+        gameMaster.pushService(ServiceType.VOTE_PHASE_START);
+
+        // TODO: 現状、ボタン連打やタイマーの競合（マルチスレッド）が発生した場合、
+        // gameMaster.pushService() が二重に発火して投票開始アナウンスが重複する懸念があります。
+        // 
+        // 【改善案】
+        // GameStateManager.java の `currentPhase` に volatile を付与した上で、
+        // DISCUSSION_ENDED の switch-case 内で直接 pushService を呼ぶ形に統一するか、
+        // もしくは check() メソッドの戻り値を boolean に変更し、初めてフェーズ変更に
+        // 成功した（AtomicBoolean の勝者）スレッドのみがこの service 側で pushService を
+        // 実行できるようにリファクタリングすることを提案します（他のVOTE_SUBMITTED等の処理と共通化）。
+
+
         // 成功を EndDiscussionResultMessage に設定して返す
         return new EndDiscussionResultMessage();
     }
